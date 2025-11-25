a2f97bd3a8b80acf1741cdff6e3a3fcae4b11651
lmms500 <lmms500@gmail.com>
2025-11-25 16:48:43 -0300

    Versão 1.1

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/App';
+import App from './App';
+import './index.css'; // <--- ESSA LINHA É OBRIGATÓRIA PARA O TAILWIND
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
